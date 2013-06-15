d5364bf923799fc0ea610f9172420f9f799b955f
Andrew Smith <andrew.smith@intelitycorp.com>
2013-06-15 00:25:57 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitFriends::Application.config.secret_key_base = 'fe3e9c67f80b859e3e09ab17d40518a75558db497904bb8adec883a27dfd85614b7d6d4e8d10b3ae441b5c8c8345871487dae5d03449a742652a068c648c70df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FitFriends::Application.config.session_store :cookie_store, key: '_FitFriends_session'
