66511331b58a22e6e8eacf7190e19ce0f13348b0
Andrew Smith <andrew.smith@intelitycorp.com>
2013-06-26 09:18:46 -0400

    Fix User#percentage_lost type safety.

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,9 +68,11 @@ class User < ActiveRecord::Base
   end
 
   def percentage_lost
-    start_weight = self.start_weight
-    kilos_lost = start_weight - self.current_weight
-    kilos_to_lose = start_weight - self.goal_weight
+    current_weight = self.current_weight || BigDecimal.new('0')
+    goal_weight = self.goal_weight || BigDecimal.new('0')
+    start_weight = self.start_weight || BigDecimal.new('0')
+    kilos_lost = start_weight - current_weight
+    kilos_to_lose = start_weight - goal_weight
     BigDecimal.new((kilos_lost / kilos_to_lose) * 100)
   end
 
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,6 +1,7 @@
 json.array!(@users) do |user|
-  json.extract! user, :id, :name, :email, :start_weight, :goal_weight, :current_weight, :height, :percentage_lost
+  json.extract! user, :id, :name, :email, :start_weight, :goal_weight, :current_weight, :height
   json.url user_url(user, format: :json)
   json.todays_carbs user.carbs_consumed
   json.todays_water user.water_consumed
+  json.percentage_lost user.percentage_lost.round(1)
 end
